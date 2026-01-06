19b16a98287d290ab161e5b82c695c28863e0acd
Vyzecb <info@vyze.nl>
2026-01-06 12:17:51 +0100

    Update contact.ts

--- api/contact.ts
+++ api/contact.ts
@@ -129,7 +129,7 @@ export default async function handler(
 
     // 📩 Mail naar jou
     await resend.emails.send({
-      from: "Vos Web Designs <onboarding@resend.dev>",
+      from: "Vos Web Designs <info@voswebdesigns.nl>",
       to: ["info@voswebdesigns.nl"],
       subject: `Nieuw contactbericht van ${data.name}`,
       html: adminTemplate(data),
@@ -137,7 +137,7 @@ export default async function handler(
 
     // ✉️ Bevestiging naar klant
     await resend.emails.send({
-      from: "Vos Web Designs <onboarding@resend.dev>",
+      from: "Vos Web Designs <info@voswebdesigns.nl>",
       to: [data.email],
       subject: "Wij hebben uw bericht ontvangen",
       html: customerTemplate(data),
